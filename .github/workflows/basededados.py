f4aebc0230d826b9c39770d34a7960bd1aa31df5
azevedokarla93 <azevedo.karla@hotmail.com>
2025-10-28 12:21:43 +0000

    Fix variable assignments and print statements

--- .github/workflows/basededados.py
+++ .github/workflows/basededados.py
@@ -1,9 +1,9 @@
 import sys
 
-servidor: sys.argv[1]
-utilizador:sys.argv[2]
-password:sys.argv[3]
+servidor = sys.argv[1]
+utilizador = sys.argv[2]
+password = sys.argv[3]
 
 print(" O servidor é:", servidor)
-print(" O utilizador é:, utilizador")
-print("A password é:, password")
+print(" O utilizador é:", utilizador)
+print("A password é:", password)
