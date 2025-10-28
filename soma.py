548a20c2b874ac01b2561ee61d555ab4f164a417
azevedokarla93 <azevedo.karla@hotmail.com>
2025-10-28 11:31:42 +0000

    Convert input arguments to integers for addition

--- soma.py
+++ soma.py
@@ -1,6 +1,6 @@
 # deve receber dois valores passados por argumento e imprimir a soma dos dois.
 import sys
 
-v1=sys.argv[1]
-v2=sys.argv[2]
+v1=int(sys.argv[1])
+v2=int(sys.argv[2])
 print("soma", v1+v2)
